869839e61f2dbfdbb934ab53a7f17f18177cfd40
Dinesha Shair <dms60488@uga.edu>
2024-05-14 22:23:46 -0400

    Node class is running.

---
+++ src/stackDS/code/Node.java
@@ -0,0 +1,54 @@
+package stackDS.code;
+
+/** Node class for stack implementation.
+ */
+public class Node<T> {
+    private T value;
+    private Node<T> next;
+
+    /** Node constructor.
+     * @param value sets the node value
+     */
+    public Node(T value) {
+        this.value = value;
+        this.next = null;
+    } // constructor.
+
+    /** Node constructor takes in two parameters to connect hjead to the list.
+     *  @param value
+     *  @param nextValue
+     */
+    public Node(T value, Node<T> nextValue) {
+        this.value = value;
+        this.next = nextValue;
+    } // Node constructor2
+
+    /** Method sets the node to its value.
+     *  @param value
+     */
+    public void setNode(T value) {
+        this.value = value;
+    } // setNode
+
+    /** method returns the current Node.
+     *  @return value of the current Node
+     */
+    public T getNode() {
+        return this.value;
+    } // getNode
+
+    /** Method sets the next Node which is a link for the list..
+     *  @param nextValue
+     */
+    public void setNext(Node<T> nextValue) {
+        this.next = nextValue;
+    } // setNext
+
+    /** Method returns the next node linked to current Node.
+     *  @return next Node
+     */
+    public Node<T> getNext() {
+        return this.next;
+    } // getNext
+
+} // Node
